f456a3714d04fa177f0a0e163696c7324cc9fb30
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2022-03-25 20:21:12 -0600

    changes to ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 import Layout from "../Components/Layout";
-import axios from 'axios'
+import axios from "axios";
 
 const IndexPage = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   const options = {
-    method: 'GET',
-    url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+    method: "GET",
+    url: "https://dad-jokes.p.rapidapi.com/random/joke",
     headers: {
-      'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com',
-      'X-RapidAPI-Key': '17fb7de893msh59a82cece7f697ap1c0305jsn0e7db5ca2d76'
-    }
+      "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+      "X-RapidAPI-Key": "17fb7de893msh59a82cece7f697ap1c0305jsn0e7db5ca2d76",
+    },
   };
-  
-    function jokes() {
-      axios.request(options).then((res) =>  {
-      console.log(res.data);
-      const data =  res.data
-      setData(data)
-    }).catch((error) => {
-      console.error(error);
-    });
+
+  function jokes() {
+    axios
+      .request(options)
+      .then((res) => {
+        console.log(res.data);
+        const data = res.data;
+        setData(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return (
@@ -30,9 +33,10 @@ const IndexPage = () => {
         <div className="center">
           <cite>Press for the best dad jokes around!</cite>
           <button onClick={jokes}> Get Jokes</button>
-
-          <h4>{data?.body?.[0]?.setup}</h4>
-          <p>{data?.body?.[0].punchline}</p>
+          <div className="card">
+            <h4>{data?.body?.[0]?.setup}</h4>
+            <p>{data?.body?.[0].punchline}</p>
+          </div>
         </div>
       </main>
     </Layout>
